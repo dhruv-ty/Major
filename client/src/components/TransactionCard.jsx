e178fe632336520a785e7bde174c95d4a0eeff9e
chiranthKk16 <chiranthkrishnak@gmail.com>
2023-12-03 11:42:44 +0530

    changes to transaction

--- client/src/components/TransactionCard.jsx
+++ client/src/components/TransactionCard.jsx
@@ -4,46 +4,37 @@ import pic from '../../images/pic.jpg';
 const TransactionCard = ({sellerName, description, date, amount, energyPurchased, profit}) => {
 
   return (        
-    <div className="flex justify-center items-center rounded-lg bg-[#26262a] mx-5">
+    <div className="flex justify-center items-center rounded-lg blue-glassmorphism mx-5">
     <div className="flex flex-col w-[350px]">
         
         <img src={pic} width={350} resizeMode={"cover"} style={{borderRadius: 10}}/>
         
-        <div className="text-white text-2xl font-bold font-sans mx-4 mt-5">
-            {sellerName}
+        <div className="flex flex-row w-full justify-between mt-5">
+            <div className="text-white text-2xl font-bold font-sans mx-4">
+                {sellerName}
+            </div>
+            <div className="text-white text-base font-sans mx-4 justify-center items-center w-fit px-3 py-1">
+                {date}
+            </div>
         </div>
         <div className="text-white text-left text-xs font-light px-4">
             {description}
         </div>
 
-        <div className="text-white text-base font-bold font-sans mx-4 mt-4 rounded-lg bg-[#097969] justify-center items-center w-fit px-3 py-1">
-            {date}
-        </div>
+        
         <div className="flex flex-row justify-between" style={{width: "95%"}}>
         <div className="text-white text-base font-bold px-4 mt-2 ">
             Energy Purchased:
         </div>
-        <div className="text-white text-base font-bold px-4 mt-4 rounded-lg bg-[#097969] justify-center items-center w-fit ">{energyPurchased}</div>
+            <div className="text-white text-base font-bold px-4 mt-2 rounded-lg bg-[#097969] justify-center items-center w-fit ">{energyPurchased} KWH</div>
         </div>
-        <div className="flex flex-row justify-between" style={{width: "95%"}}>
-        <div className="text-white text-base font-bold px-4 mt-2 ">
-            Amount Paid:
-        </div>
-        <div className="text-white text-base font-bold px-4 mt-4 rounded-lg bg-[#097969] justify-center items-center w-fit ">{amount}</div>
-        </div>
-        <div className="flex flex-row justify-between" style={{width: "95%"}}>
-        <div className="text-white text-base font-bold px-4 mt-2 ">
-            Profit Gained:
-        </div>
-        <div className="text-white text-base font-bold px-4 mt-4 rounded-lg bg-[#097969] justify-center items-center w-fit ">{profit}</div>
-        </div>
-        <div className="flex flex-row w-full justify-between text-white text-base font-bold mt-4">
-            <div></div>
-            <div className="items-end justify-end">
-            <button type="button" className="text-white text-xs text-right justify-center w-fit items-center mr-4 px-4 mb-4 bg-[#2952e3] py-2 rounded-full cursor-pointer hover:bg-[#2546bd]">
-                More
+    
+        
+        <div className="flex flex-row w-full text-white mt-4">            
+            <button type="button" className="mx-4 text-white text-xs text-right justify-center w-fit items-center mr-4 px-4 mb-4 bg-[#2952e3] py-2 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                See More
             </button>
-            </div>
+            
         </div>
 
     </div>    
--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import TransactionCard from "./TransactionCard";
 const Transactions = () => {
   const transactionsData = [
-    { id: 1, sellerName: 'Seller 1', description: 'Transaction 1', date: '2023-01-01', amount: 100, energyPurchased: 500, profit: 20 },
-    { id: 2, sellerName: 'Seller 2', description: 'Transaction 2', date: '2023-02-01', amount: 150, energyPurchased: 700, profit: 30 },
-    { id: 3, sellerName: 'Seller 3', description: 'Transaction 3', date: '2023-03-01', amount: 180, energyPurchased: 800, profit: 40 },
-    { id: 4, sellerName: 'Seller 4', description: 'Transaction 4', date: '2023-04-01', amount: 120, energyPurchased: 200, profit: 50 },
-    { id: 5, sellerName: 'Seller 5', description: 'Transaction 5', date: '2023-05-01', amount: 250, energyPurchased: 100, profit: 60 },
-    { id: 6, sellerName: 'Seller 6', description: 'Transaction 6', date: '2023-06-01', amount: 110, energyPurchased: 900, profit: 70 },
+    { id: 1, sellerName: 'Seller 1', description: 'Transaction 1', date: '01/02/2023',  energyPurchased: 500},
+    { id: 2, sellerName: 'Seller 2', description: 'Transaction 2', date: '01/02/2023',  energyPurchased: 700},
+    { id: 3, sellerName: 'Seller 3', description: 'Transaction 3', date: '01/02/2023',  energyPurchased: 800 },
+    { id: 4, sellerName: 'Seller 4', description: 'Transaction 4', date: '01/02/2023', energyPurchased: 200},
+    { id: 5, sellerName: 'Seller 5', description: 'Transaction 5', date: '01/02/2023', energyPurchased: 100},
+    { id: 6, sellerName: 'Seller 6', description: 'Transaction 6', date: '01/02/2023',  energyPurchased: 900},
     
   ];
   return (        
@@ -19,10 +19,8 @@ const Transactions = () => {
           key={transaction.id}
           sellerName={transaction.sellerName}
           description={transaction.description}
-          date={transaction.date}
-          amount={transaction.amount}
-          energyPurchased={transaction.energyPurchased}
-          profit={transaction.profit}
+          date={transaction.date}          
+          energyPurchased={transaction.energyPurchased}        
           />
         ))}           
     </div>
